de85dc71dbad9dad0b16ba73e6ac8c7bed21711c
Celso Henrique <celso.henrique@hotmail.com.br>
2019-08-16 16:38:55 -0300

    Adding jest as the test runner

---
+++ src/app.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import App from './app'
+
+describe('App', () => {
+  it('renders without crash', () => {
+    const { getByText } = render(<App />)
+
+    expect(getByText('App loaded')).toBeTruthy()
+  })
+})
