14d38108b1a1d9be3795f4a4d1262d157e66f61c
Celso Henrique <celso.henrique@hotmail.com.br>
2019-08-16 17:15:17 -0300

    Adds an eslint rule and changes the files extensions

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    semi: 0
+    semi: 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.jsx',
   module: {
     rules: [
       {
@@ -20,9 +19,6 @@ module.exports = {
         ]
       }
     ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
   },
   plugins: [
     new HtmlWebPackPlugin({
