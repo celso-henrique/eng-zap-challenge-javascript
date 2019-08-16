67ba205a349a74136b3c19ce6c6b8bba248ef863
Celso Henrique <celso.henrique@hotmail.com.br>
2019-08-16 16:17:40 -0300

    Adding resolve extensions to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  entry: './src/index.jsx',
   module: {
     rules: [
       {
@@ -19,6 +20,9 @@ module.exports = {
         ]
       }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
   },
   plugins: [
     new HtmlWebPackPlugin({
