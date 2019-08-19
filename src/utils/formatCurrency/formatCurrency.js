e291a5b03e393037d06ce8c949a3f8673aebea37
Celso Henrique <celso.henrique@hotmail.com.br>
2019-08-19 10:00:24 -0300

    Fixing lint

--- src/utils/formatCurrency/formatCurrency.js
+++ src/utils/formatCurrency/formatCurrency.js
@@ -1,7 +1,6 @@
-const formatCurrency = value =>
-  parseFloat(value).toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: 'BRL'
-  })
+const formatCurrency = value => parseFloat(value).toLocaleString('pt-BR', {
+  style: 'currency',
+  currency: 'BRL'
+})
 
 export default formatCurrency
